da866de894ad11cdcb1ef3bfbc9a2ce8f6df8a35
Lars Greiss <kufikugel@googlemail.com>
2014-01-28 23:49:48 +0100

    Revert "ARM: Change dmb domain for bionic_atomic_barrier()"
    Well the original change is indeed correct. But there are devices
    which are doing weird things in their proprietaries across gpu and other memory areas
    (toro and toroplus as example). This ends up in serious issues on this devices
    which are doing it obviosuly wrong.
    
    Whatever till we know which devices are all effected and we can ifdef them
    we revert it for now.
    
    This reverts commit f8caadb87dfc4f2eed86462fa6fb0cab5262d52e.
    
    Change-Id: I93ba5dff94ebdef4629d7d9f005037b190e76ad4

--- libc/private/bionic_atomic_arm.h
+++ libc/private/bionic_atomic_arm.h
@@ -16,9 +16,9 @@
 #ifndef BIONIC_ATOMIC_ARM_H
 #define BIONIC_ATOMIC_ARM_H
 
-__ATOMIC_INLINE__ void __bionic_memory_barrier() {
+__ATOMIC_INLINE__ void __bionic_memory_barrier(void) {
 #if defined(ANDROID_SMP) && ANDROID_SMP == 1
-  __asm__ __volatile__ ( "dmb ish" : : : "memory" );
+  __asm__ __volatile__ ( "dmb" : : : "memory" );
 #else
   /* A simple compiler barrier. */
   __asm__ __volatile__ ( "" : : : "memory" );
